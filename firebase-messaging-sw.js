f63eb693a5ba368468ae3fcbbf9b22581269ef37
javivilchis <javivilchis@gmail.com>
2021-05-12 17:14:50 -0500

    removing

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,12 +3,12 @@ importScripts('https://www.gstatic.com/firebasejs/6.3.0/firebase-messaging.js');
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyB6dAkDfLqA790njRSb64IAB9-BmPI6fJY",
+    apiKey: "*******************",
     authDomain: "push-test-jav.firebaseapp.com",
     databaseURL: "https://push-test-jav.firebaseio.com",
     projectId: "push-test-jav",
     storageBucket: "push-test-jav.appspot.com",
-    messagingSenderId: "971122330221",
+    messagingSenderId: "*************",
     appId: "1:971122330221:web:90872652bf6e2c6b"
 };
 // Initialize Firebase    
